c4fa5ffc10b5841a13ecd3002d6dac81766f6c0d
Khan, Owais <A0076323@kiongroup.com>
2025-09-17 12:46:42 -0400

    Added swagger code to open swagger on browser.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-// builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
